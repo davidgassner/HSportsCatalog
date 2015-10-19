945284f9d4e27a3076eef67eecb781cb2d774c28
David Gassner <david.gassner@gmail.com>
2015-10-19 16:27:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/hsports/ClothingItem.java
@@ -0,0 +1,17 @@
+package com.example.android.hsports;
+
+public class ClothingItem {
+
+    private String itemId;
+    private String name;
+    private String description;
+    private double price;
+
+    public ClothingItem(String itemId, String name, String description, double price) {
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+}
