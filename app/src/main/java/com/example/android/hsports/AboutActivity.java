cd72d57f7974ed31bb251f085cadee060d42679e
David Gassner <david.gassner@gmail.com>
2015-10-20 14:01:58 -0700

    Added About activity, renamed from H+ Sports to H+ Sport

--- app/src/main/java/com/example/android/hsports/AboutActivity.java
+++ app/src/main/java/com/example/android/hsports/AboutActivity.java
@@ -1,12 +1,9 @@
 package com.example.android.hsports;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
