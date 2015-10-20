e91480fcdac2b6736e5ca19b68ccf51ffcafde1d
David Gassner <david.gassner@gmail.com>
2015-10-19 17:14:00 -0700

    Added PNG files to drawable, minor code changes

--- app/src/main/java/com/example/android/hsports/MainActivity.java
+++ app/src/main/java/com/example/android/hsports/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.android.hsports;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import java.util.List;
-import java.util.Map;
+
+//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,42 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setTitle(getString(R.string.main_activity_title));
 
         List<ClothingItem> itemList = DataProvider.itemList;
-        Map<String, ClothingItem> itemMap = DataProvider.itemMap;
+//        Map<String, ClothingItem> itemMap = DataProvider.itemMap;
 
-        String msg = "List items: " + itemList.size() + ", map items: " + itemMap.size();
-        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//        String msg = "List items: " + itemList.size() + ", map items: " + itemMap.size();
+//        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
